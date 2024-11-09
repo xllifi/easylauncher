2a7d2865cb18709ee6144c24b6edd14ed856551f
xllifi <unicoded@ya.ru>
2024-11-09 10:42:06 +1000

    generalise input-text style

--- src/renderer/src/lib/settings/components/OptionTextInput.svelte
+++ src/renderer/src/lib/settings/components/OptionTextInput.svelte
@@ -34,28 +34,5 @@
       min-width: 0;
       flex: 1 1 0;
     }
-
-    input[type="text"] {
-      min-width: 0;
-      width: 4rem;
-      max-width: 12rem;
-      flex: 1 1 0;
-      
-      height: 1.25rem;
-      background-color: #0006;
-      color: var(--text-color);
-      font-family: Inter;
-      font-weight: 460;
-      padding: 0 0.2rem;
-      border: none;
-      border-radius: 0.2rem;
-      outline: solid var(--theme-accent-inactive) 2px;
-
-      transition: outline-color 100ms, filter 100ms;
-      &:focus {
-        outline: solid var(--theme-accent-active) 2px;
-        filter: drop-shadow(0 0 3px var(--theme-accent-active-darker));
-      }
-    }
   }
 </style>
