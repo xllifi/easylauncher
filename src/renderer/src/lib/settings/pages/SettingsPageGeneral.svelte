f77d082191bd2cf6d40c3599a95e10d692713d7a
xllifi <unicoded@ya.ru>
2024-11-09 14:36:53 +1000

    rename option components

--- src/renderer/src/lib/settings/pages/SettingsPageGeneral.svelte
+++ src/renderer/src/lib/settings/pages/SettingsPageGeneral.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import Checkbox from "../components/OptionCheckbox.svelte"
-  import TextInput from "../components/OptionTextInput.svelte"
+  import OptionCheckbox from "../components/OptionCheckbox.svelte"
+  import OptionTextInput from "../components/OptionTextInput.svelte"
   import { params } from "../params"
 </script>
 
-<TextInput name="Никнейм" bind:input={$params.username}/>
-<Checkbox name="Checkbox (not persistent)" />
-<Checkbox name="Checkbox (persistent)" bind:checked={$params.testopt}/>
+<OptionTextInput name="Никнейм" bind:input={$params.username}/>
+<OptionCheckbox name="Checkbox (not persistent)" />
+<OptionCheckbox name="Checkbox (persistent)" bind:checked={$params.testopt}/>
