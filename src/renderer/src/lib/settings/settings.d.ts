6841be69a13731eefb04adaeed766a735ce8d8b8
xllifi <unicoded@ya.ru>
2024-11-12 11:44:44 +1000

    remove unused types

--- src/renderer/src/lib/settings/settings.d.ts
+++ src/renderer/src/lib/settings/settings.d.ts
@@ -1,16 +1,5 @@
-export type Settings = {
-  pages: SettingsPage[]
-}
-
 export type SettingsPage = {
   id: string,
   title: string,
   options: SettingsPageOption[]
-}
-
-export type SettingsPageOption = {
-  id: string,
-  name: string,
-  binds_to: string,
-  type: string
 }
