1f52ebdb5eacabcb0f5c099bbc64bcc620ceed22
xllifi <unicoded@ya.ru>
2024-11-25 23:14:10 +1000

    create OptionButton and use it in SettingsPageGeneral

--- src/renderer/src/lib/settings/pages/SettingsPageGeneral.svelte
+++ src/renderer/src/lib/settings/pages/SettingsPageGeneral.svelte
@@ -2,8 +2,10 @@
   import OptionCheckbox from "../components/OptionCheckbox.svelte"
   import OptionTextInput from "../components/OptionTextInput.svelte"
   import { params } from "../../stores/params"
+  import OptionButton from "../components/OptionButton.svelte"
 </script>
 
 <OptionTextInput name="Никнейм" bind:input={$params.username}/>
 <OptionCheckbox name="Checkbox (not persistent)" />
 <OptionCheckbox name="Checkbox (persistent)" bind:checked={$params.testopt}/>
+<OptionButton name="Вход в аккаунт" actionLabel="Войти!" onclick={() => console.log('cliccked')}/>
