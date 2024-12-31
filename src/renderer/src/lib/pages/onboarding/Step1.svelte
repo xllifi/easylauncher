1d8e9771d6e0d904b1547fadd2921b3bd6458a17
xllifi <unicoded@ya.ru>
2025-01-01 01:35:10 +1000

    Remove menu button in onboarding>step1

--- src/renderer/src/lib/pages/onboarding/Step1.svelte
+++ src/renderer/src/lib/pages/onboarding/Step1.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import type { MouseEventHandler } from "svelte/elements"
-  import { route } from "../../stores/route.svelte.js"
 
   interface Props {
     next: MouseEventHandler<any>
@@ -11,4 +10,3 @@
 <h1>Привет!</h1>
 <p>Давай всё настроим...</p>
 <button onclick={next}>Далее</button>
-<button onclick={() => {$route.page = 'main'}}>Похуй, в меню меня</button>
