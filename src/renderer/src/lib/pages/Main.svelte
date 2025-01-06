358623766d80a780e8afd4d1538bffb979f7ab63
xllifi <unicoded@ya.ru>
2025-01-06 18:25:37 +1000

    improve skin render

--- src/renderer/src/lib/pages/Main.svelte
+++ src/renderer/src/lib/pages/Main.svelte
@@ -9,6 +9,7 @@
   import { onMount } from 'svelte'
   import * as skinview3d from 'skinview3d'
   import { getSkinUrls } from './Main.svelte.js'
+  import * as THREE from 'three'
   import noskin from '../../assets/unknownplayer.png'
   let statusBar, skinCv: HTMLCanvasElement, skinVw: skinview3d.SkinViewer
   let skin: string = noskin
@@ -16,12 +17,13 @@
   let skinLoaded: boolean = false
 
   function resizeCv(cv: HTMLCanvasElement, sw: skinview3d.SkinViewer) {
+    if (!cv || !sw) return
     const height = window.innerHeight
-    const width = height / 2
+    const width = height / 1.3
 
     cv.width = width
-    sw.width = width
     cv.height = height
+    sw.width = width
     sw.height = height
   }
 
@@ -34,11 +36,17 @@
       canvas: skinCv,
       skin,
       cape,
-      animation: new skinview3d.IdleAnimation()
+      animation: new skinview3d.IdleAnimation(),
+      pixelRatio: window.devicePixelRatio * 2
     })
 
+    // Scene settings
+
+    const player = skinVw.playerObject
+    player.rotateY(0.45)
+
     const cam = skinVw.camera
-    cam.position.set(-20, 3, 28)
+    cam.position.z = 28
     const ctrl = skinVw.controls
     ctrl.target.set(0, 7, 0)
     ctrl.enableDamping = true
@@ -47,6 +55,47 @@
     ctrl.minPolarAngle = 1.65
     ctrl.maxPolarAngle = 1.65
 
+    const renderer = skinVw.renderer
+    let lastInteractTimestamp = 0
+    let smoothReset: boolean = false
+    ctrl.addEventListener('start', onStart)
+    ctrl.addEventListener('end', onEnd)
+
+    function onStart() {
+      ctrl.minAzimuthAngle = -Infinity
+      ctrl.maxAzimuthAngle = Infinity
+      smoothReset = false
+    }
+    function onEnd() {
+      lastInteractTimestamp = Date.now()
+      smoothReset = true
+    }
+    function animate() {
+      if (!smoothReset || lastInteractTimestamp + 1500 > Date.now()) return
+      // get current angle
+      var alpha = ctrl.getAzimuthalAngle()
+
+      // smooth change using manual lerp
+      ctrl.minAzimuthAngle = 0.98 * alpha
+      ctrl.maxAzimuthAngle = ctrl.minAzimuthAngle
+
+      // if the reset values are reached, exit smooth reset
+      if (alpha == 0) onStart()
+    }
+    renderer.setAnimationLoop(animate)
+
+    // Dramatic lights
+    skinVw.globalLight.visible = false
+    const rectLight = new THREE.RectAreaLight(0x404040, 20, 20, 40)
+    rectLight.position.set(-9, 0, -27)
+    rectLight.lookAt(0, 0, -27)
+    cam.add(rectLight)
+
+    const light = new THREE.PointLight(0xffffff, 60)
+    light.position.set(12, 8, -18)
+    cam.add(light)
+
+    // Render
     resizeCv(skinCv, skinVw)
     window.addEventListener('resize', () => {
       resizeCv(skinCv, skinVw)
@@ -57,15 +106,25 @@
   async function changeSkin(retriesLeft?: number) {
     skinLoaded = false
     let skinval = await getSkinUrls()
-    if (skinval.skin) skin = skinval.skin
-    else skin = noskin
-    if (skinval.cape) cape = skinval.cape
-
+    if (skinval.skin) {
+      skin = skinval.skin
+    } else {
+      skin = noskin
+    }
     skinVw.loadSkin(skin)
-    skinVw.loadCape(cape)
 
-    if ((!window.navigator.onLine || skin == noskin) && (retriesLeft && retriesLeft > 0)) {
-      return changeSkin(retriesLeft ? retriesLeft - 1 : 2)
+    if (skinval.cape) {
+      cape = skinval.cape
+      skinVw.loadCape(cape)
+    } else {
+      skinVw.resetCape()
+    }
+
+    if ((!window.navigator.onLine || skin == noskin) && retriesLeft && retriesLeft > 0) {
+      setTimeout(() => {
+        changeSkin(retriesLeft ? retriesLeft - 1 : 2)
+      }, 250)
+      return
     }
 
     skinLoaded = true
@@ -116,16 +175,18 @@
   })
 </script>
 
-<svelte:window ononline={() => {
-  setTimeout(() => {
-    changeSkin()
-  }, 2000);
-}} />
+<svelte:window
+  ononline={() => {
+    setTimeout(() => {
+      changeSkin()
+    }, 2000)
+  }}
+/>
 
 <!-- svelte-ignore element_invalid_self_closing_tag -->
 <canvas class="skin" class:noskin={skin == noskin} class:hidden={!skinLoaded} bind:this={skinCv} />
 <div class="main">
-  <button onclick={launchGame} class="start">Start</button>
+  <button onclick={launchGame}>Start</button>
   <button onclick={() => ($route.overlay.current = 'settings')}>Settings</button>
   <!-- <button onclick={() => ($route.overlay.current = 'login')}>Login</button> -->
   <StatusBar bind:this={statusBar} />
@@ -152,12 +213,11 @@
   .skin {
     opacity: 1;
     position: absolute;
-    left: 0;
+    left: -8dvw;
     bottom: 0;
 
     transition: opacity 500ms;
 
-    filter: brightness(50%);
     &.hidden {
       opacity: 0;
     }
--- src/renderer/src/lib/pages/Main.svelte.ts
+++ src/renderer/src/lib/pages/Main.svelte.ts
@@ -1,23 +1,18 @@
-import ky from "ky"
-import { get } from "svelte/store"
-import { params } from "../stores/params.svelte.js"
+import ky from 'ky'
+import { get } from 'svelte/store'
+import { params } from '../stores/params.svelte.js'
 
 export async function getSkinUrls() {
-  let ret: getSkinReturn
-  try {
-    const skin: sessionResp = await ky.get(`https://testauth.xllifi.ru/session/minecraft/profile/${get(params).launchCredentials.uuid}`, {
-      timeout: 3000
-    }).json()
-    const skinvalues: texturesValue = JSON.parse(atob(skin.properties.filter((x) => x.name == 'textures')[0].value))
+  const uuid = get(params).launchCredentials.uuid
+  if (!uuid) return {} as getSkinReturn
 
-    ret = {
-      skin: skinvalues.textures.SKIN ? skinvalues.textures.SKIN.url : null,
-      cape: skinvalues.textures.CAPE ? skinvalues.textures.CAPE.url : null
-    }
-  } catch {
-    ret = {} as getSkinReturn
+  const skin: sessionResp = await ky.get(`https://testauth.xllifi.ru/session/minecraft/profile/${uuid}`, { timeout: 3000 }).json()
+  const skinvalues: texturesValue = JSON.parse(atob(skin.properties.filter((x) => x.name == 'textures')[0].value))
+
+  return {
+    skin: skinvalues.textures.SKIN ? skinvalues.textures.SKIN.url : null,
+    cape: skinvalues.textures.CAPE ? skinvalues.textures.CAPE.url : null
   }
-  return ret
 }
 
 export type getSkinReturn = {
