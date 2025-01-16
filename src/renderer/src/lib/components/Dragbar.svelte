4b6357d20aa9b5c47cd97d2963509dcd3e8ab004
xllifi <unicoded@ya.ru>
2025-01-16 17:49:01 +1000

    improve dragbar

--- src/renderer/src/lib/components/Dragbar.svelte
+++ src/renderer/src/lib/components/Dragbar.svelte
@@ -2,6 +2,7 @@
   import { X, Minus, Bug } from 'lucide-svelte'
   import { ipc } from '../scripts/general.js'
   import { _ } from 'svelte-i18n'
+  import { route } from '../stores/route.svelte.js'
 
   function quit(): void {
     ipc.send('quit')
@@ -19,11 +20,8 @@
     <p>{$_('dragbar.title')} • {$_('dragbar.prealpha')}</p>
   </div>
   <div class="buttons">
-    <span class="divider"></span>
     <button class="report" on:click|stopPropagation={report}><Bug width="18px" height="18px" /></button>
-    <span class="divider"></span>
     <button class="minimize" on:click|stopPropagation={minimize}><Minus width="18px" height="18px" /></button>
-    <span class="divider"></span>
     <button class="close" on:click|stopPropagation={quit}><X width="18px" height="18px" /></button>
   </div>
 </div>
@@ -36,26 +34,12 @@
 
     position: relative;
     -webkit-app-region: drag;
-    height: 1.5rem;
+    height: 2rem;
     width: 100%;
     background-color: var(--color-background-lighter);
 
     display: flex;
     flex-direction: row;
-
-    .divider {
-      width: 1.1px;
-      height: 1.5rem;
-      background-color: var(--color-border);
-    }
-    &::after {
-      content: '';
-      position: absolute;
-      bottom: -1px;
-      height: 1.1px;
-      width: 100%;
-      background-color: var(--color-border);
-    }
 
     .text {
       display: inline-flex;
@@ -64,30 +48,29 @@
       font-family: Inter;
       font-weight: 500;
 
-      height: 1.5rem;
       align-items: center;
 
-      padding-left: 0.5rem;
+      padding-left: 0.75rem;
       color: var(--color-border);
     }
     .buttons {
       display: inline-flex;
       flex-direction: row;
+      margin: 0.25rem;
       margin-left: auto;
-
-      height: 1.5rem;
+      gap: 0.5rem;
 
       button {
         -webkit-app-region: no-drag;
         position: relative;
-        width: 2rem;
+        width: 1.5rem;
         height: 1.5rem;
-        border-radius: 0;
 
         margin: 0;
         padding: 0;
 
         border: none;
+        outline: none;
         background-color: transparent;
         cursor: pointer;
 
@@ -104,33 +87,13 @@
           padding: 0;
           margin: 0;
           color: var(--color-text-primary);
-          // transition:
-          //   transform 100ms ease-in-out,
-          //   filter 200ms ease-in-out;
-        }
-
-        &:is(:hover, :focus-visible) {
-          outline: none;
-          filter: none;
+          opacity: 0.4;
+          transition: opacity 100ms;
         }
 
         &:is(:hover, :focus-visible) :global(.lucide) {
-          transform: scale(1.1);
-          color: #000;
+          opacity: 1;
         }
-
-        &:active :global(.lucide) {
-          transform: scale(1);
-        }
-      }
-      .close:is(:hover, :focus-visible) {
-        background-color: #b33;
-      }
-      .minimize:is(:hover, :focus-visible) {
-        background-color: #ccc;
-      }
-      .report:is(:hover, :focus-visible) {
-        background-color: #33b;
       }
     }
   }
