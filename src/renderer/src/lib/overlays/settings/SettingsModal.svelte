xllifi/easylauncher

commit 7c12f5a0ab79c1454ffec843b03ff9666b7873b9

change localisation rendering on settings page

src/renderer/src/lib/overlays/settings/SettingsModal.svelte
@@ -16,11 +16,11 @@
   const pages = $state([
     {
       component: 'SettingsPageGeneral',
-      title: $_('modal.settings.pages.general.tab')
+      title: 'modal.settings.pages.general.tab'
     },
     {
       component: 'SettingsPageLaunch',
-      title: $_('modal.settings.pages.launch.tab')
+      title: 'modal.settings.pages.launch.tab'
     }
   ])
 
@@ -33,7 +33,7 @@
       case 'SettingsPageLaunch':
         return (Page = SettingsPageLaunch)
       default:
-        pageTitle = $_('modal.settings.pages.404.tab')
+        pageTitle = 'modal.settings.pages.404.tab'
         return (Page = SettingsPage404)
     }
   }
@@ -46,7 +46,7 @@
 <div class="layout">
   <div class="title">
     {#key pageTitle}
-      <h2 class="text">{$_('modal.settings.title')} <span class="arrow">/</span> <span in:tilt={{ duration: 200 }}>{pageTitle}</span></h2>
+      <h2 class="text">{$_('modal.settings.title')} <span class="arrow">/</span> <span in:tilt={{ duration: 200 }}>{$_(pageTitle)}</span></h2>
     {/key}
     <button class="close" onclick={exit}>
       <X />

commit 1156d4f0eaff1d32d898aa71ef30dc2b79557ba3

fix bug caused by 7c12f5a0

src/renderer/src/lib/overlays/settings/SettingsModal.svelte
@@ -54,7 +54,7 @@
   </div>
   <div class="left">
     {#each pages as page}
-      <button tabindex="0" class="tab" id={page.component} onclick={changePage} class:selected={currentPageComponentName === page.component}>{page.title}</button>
+      <button tabindex="0" class="tab" id={page.component} onclick={changePage} class:selected={currentPageComponentName === page.component}>{$_(page.title)}</button>
     {/each}
   </div>
   {#key Page}